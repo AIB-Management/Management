f4dfff6b40ce6eabeb8e18735f2a88ec3c1be838
MaHanZhen <184999894@qq.com>
2017-05-06 13:00:36 +0800

    添加发送邮件功能

--- src/main/java/com/gdaib/service/MailService.java
+++ src/main/java/com/gdaib/service/MailService.java
@@ -1,10 +1,12 @@
 package com.gdaib.service;
 
+import com.gdaib.pojo.MailPojo;
+
 /**
  * @Author:马汉真
  * @Date: 17-5-6
  * @role:
  */
 public interface MailService {
-    public void sendMail();
+    public void sendAttachMail(MailPojo mail);
 }
--- src/main/java/com/gdaib/service/impl/MailServiceImpl.java
+++ src/main/java/com/gdaib/service/impl/MailServiceImpl.java
@@ -1,16 +1,84 @@
 package com.gdaib.service.impl;
 
+import com.gdaib.pojo.MailPojo;
 import com.gdaib.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.logging.Logger;
 
 /**
  * @Author:马汉真
  * @Date: 17-5-6
  * @role:
  */
-public class MailServiceImpl implements MailService{
+public class MailServiceImpl implements MailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private MimeMessage mimeMessage;
+
+
+
+    private static Logger logger = Logger.getLogger("MailServiceImpl.class");
+
+    /**
+     * 发送html格式的，带附件的邮件
+     */
     @Override
-    public void sendMail() {
-        
+    public void sendAttachMail(MailPojo mail) {
+        try {
+            MimeMessageHelper mailMessage = new MimeMessageHelper(
+                    this.mimeMessage, true, "UTF-8");
+            mailMessage.setFrom(mail.getFromAddress());// 设置邮件消息的发送者
+
+            mailMessage.setSubject(mail.getSubject());// 设置邮件消息的主题
+            mailMessage.setSentDate(new Date());// 设置邮件消息发送的时间
+            mailMessage.setText(mail.getContent(), true); // 设置邮件正文，true表示以html的格式发送
+
+            String[] toAddresses = mail.getToAddresses().split(";");// 得到要发送的地址数组
+            for (int i = 0; i < toAddresses.length; i++) {
+                mailMessage.setTo(toAddresses[i]);
+            /*
+            *  for (String fileName : mail.getAttachFileNames()) {
+            *   mailMessage.addAttachment(fileName, new File(fileName));
+            *  }
+            * */
+                // 发送邮件
+
+
+                mailSender.send(mimeMessage);
+                logger.info("send mail ok=" + toAddresses[i]);
+            }
+
+        } catch (Exception e) {
+            logger.info(e.toString());
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public JavaMailSender getMailSender() {
+        return mailSender;
+    }
+
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public MimeMessage getMimeMessage() {
+        return mimeMessage;
+    }
+
+    public void setMimeMessage(MimeMessage mimeMessage) {
+        this.mimeMessage = mimeMessage;
     }
 }
