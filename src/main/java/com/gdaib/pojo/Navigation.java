c0ce52f3eac0254759fff5e902b094f7bde14175
MaHanZhen <184999894@qq.com>
2017-05-09 17:29:37 +0800

    添加nav类

---
+++ src/main/java/com/gdaib/pojo/Navigation.java
@@ -0,0 +1,53 @@
+package com.gdaib.pojo;
+
+public class Navigation {
+    private Integer id;
+
+    private Integer departmentId;
+
+    private Integer parent;
+
+    private String title;
+
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getParent() {
+        return parent;
+    }
+
+    public void setParent(Integer parent) {
+        this.parent = parent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+}
