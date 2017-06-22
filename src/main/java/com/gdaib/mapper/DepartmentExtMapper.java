c9edb229dfc033b8b354bfb7aada8136762c8f82
MaHanZhen <184999894@qq.com>
2017-06-22 09:56:42 +0800

    添加注释

--- src/main/java/com/gdaib/mapper/DepartmentExtMapper.java
+++ src/main/java/com/gdaib/mapper/DepartmentExtMapper.java
@@ -14,14 +14,17 @@ import java.util.List;
  */
 public interface DepartmentExtMapper extends DepartmentMapper {
 
+    //删除系别或专业
     public int deleteDepartment(@Param("uids") List<String> uids) throws Exception;
 
+    //更新系别或专业名
     public int updateDepartment(DepartmentSelectVo department) throws Exception;
 
+    //查找系别信息
     public List<DepartmentCustom> selectDepartment(DepartmentSelectVo department) throws Exception;
 
+    //查找专业信息
     public List<HashMap<String, Object>> selectProfessional(DepartmentSelectVo department) throws Exception;
-
 
     //获取某个系别的专业总数
     public DepartmentCustom getCountProfessional(DepartmentSelectVo department) throws Exception;
